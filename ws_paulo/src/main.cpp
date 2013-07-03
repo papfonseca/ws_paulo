ce3967e8be26ec5a0f30421a3a81513800970c24
paulofonseca <paulofonseca@paulofonseca-PC.(none)>
2013-07-03 16:55:17 +0100

    paulo

--- ws_paulo/src/main.cpp
+++ ws_paulo/src/main.cpp
@@ -3,9 +3,11 @@
 
 #include <sstream>
 #include <ws_referee/custom.h>
+#include <visualization_msgs/Marker.h>
 
 std::string _name = "Paulo";
 ros::Publisher chatter_pub;
+ros::Publisher marker_pub;
 double _pos_x;
 double _pos_y;
 
@@ -15,12 +17,41 @@ void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 	_pos_x += msg_in->dist;
 	
 	ws_referee::custom msg_out;
-	msg_out.dist = 0.8;
+	msg_out.dist = 0.02;
 	msg_out.sender = _name;
 	msg_out.winner = "";
 
 	ROS_INFO("%s: Iam at %f. I will publish a message", _name.c_str(), _pos_x);
   chatter_pub.publish(msg_out);
+
+	visualization_msgs::Marker marker;
+	marker.header.frame_id = "world";
+	marker.header.stamp = ros::Time();
+	marker.ns = "";
+	marker.id = 0;
+	marker.type = visualization_msgs::Marker::SPHERE;
+	marker.action = visualization_msgs::Marker::ADD;
+	marker.pose.position.x = _pos_x;
+	marker.pose.position.y = _pos_y;
+	marker.pose.position.z = 0;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
+	marker.scale.x = 0.3;
+	marker.scale.y = 0.3;
+	marker.scale.z = 0.3;
+	marker.color.a = 1.0;
+	marker.color.r = 1.0;
+	marker.color.g = 1.0;
+	marker.color.b = 0.0;
+	marker_pub.publish( marker );
+
+	marker.id = 1;
+	marker.pose.position.x = _pos_x - 0.3;
+	marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+	marker.text = "Paulo";
+	marker_pub.publish(marker);
 }
 
 
@@ -29,6 +60,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "ws_p");
   ros::NodeHandle n;
   chatter_pub = n.advertise<ws_referee::custom>("player_out", 1);
+  marker_pub = n.advertise<visualization_msgs::Marker>("paulo_marker", 1);
   ros::Subscriber sub = n.subscribe("player_in", 1, chatterCallback);
 
 	_pos_x = 0;
