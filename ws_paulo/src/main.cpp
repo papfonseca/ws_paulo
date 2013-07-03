84414b38c826b0c4e868e5e14fd84d51b24baa14
paulofonseca <paulofonseca@paulofonseca-PC.(none)>
2013-07-03 15:53:11 +0100

    paulo

--- ws_paulo/src/main.cpp
+++ ws_paulo/src/main.cpp
@@ -6,17 +6,20 @@
 
 std::string _name = "Paulo";
 ros::Publisher chatter_pub;
+double _pos_x;
+double _pos_y;
 
 void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 {
-  ROS_INFO("%s: Received msg", _name.c_str());
+  ROS_INFO("%s: Received msg with dist %f", _name.c_str(), msg_in->dist);
+	_pos_x += msg_in->dist;
 	
 	ws_referee::custom msg_out;
-	msg_out.dist = 0.8;
+	msg_out.dist = rand();
 	msg_out.sender = _name;
 	msg_out.winner = "";
 
-	ROS_INFO("%s: will publish a message", _name.c_str());
+	ROS_INFO("%s: Iam at %f. I will publish a message", _name.c_str(), _pos_x);
   chatter_pub.publish(msg_out);
 }
 
@@ -27,6 +30,9 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   chatter_pub = n.advertise<ws_referee::custom>("player_out", 1);
   ros::Subscriber sub = n.subscribe("player_in", 1, chatterCallback);
+
+	_pos_x = 0;
+	_pos_y = 5;
 
   ROS_INFO("%s: node started", _name.c_str());
 
